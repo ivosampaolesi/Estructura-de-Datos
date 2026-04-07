88c3fb12a3fde8d26fa0eeb8a16b159a3adc7dd1
Ivo <ivodribe@gmail.com>
2026-04-07 11:52:57 -0300

    Falta lo del procedimiento de listar

--- pryEDSampaolesiI/clsArchivoTexto.cs
+++ pryEDSampaolesiI/clsArchivoTexto.cs
@@ -14,19 +14,19 @@ namespace pryEDSampaolesiI
 
         public void Grabar()
         {
-            StreamWriter AD = new StreamWriter(NombreArchivo);
+            StreamWriter AD = new StreamWriter(NombreArchivo,true);
             AD.WriteLine("Hola");
             AD.Close();
         }
         public void Grabar(String Dato)
         {
-            StreamWriter AD = new StreamWriter(NombreArchivo); 
+            StreamWriter AD = new StreamWriter(NombreArchivo,true); 
             AD.WriteLine(Dato);
             AD.Close(); 
         }
         public void Grabar(String Codigo, String Nombre)
         {
-            StreamWriter AD = new StreamWriter(NombreArchivo);
+            StreamWriter AD = new StreamWriter(NombreArchivo,true);
             AD.Write(Codigo);
             AD.Write(";");
             AD.WriteLine(Nombre);
